f93598aae958e8d4298d682d5943d0b70d084cef
Jeremia Flammer <jeremiaflammer@C4LJ3PT6CKF6N.local>
2026-04-02 14:11:49 +0200

    Disable grid overlay and deepen culture teaser offset

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,8 +4,6 @@ import { notFound } from 'next/navigation'
 import { hasLocale } from 'next-intl'
 import { setRequestLocale } from 'next-intl/server'
 import { routing } from '@/lib/i18n/routing'
-import { GridOverlayProvider } from '@/components/grid/GridOverlayProvider'
-import GridOverlay from '@/components/grid/GridOverlay'
 import Navigation from '@/components/layout/Navigation'
 import Footer from '@/components/layout/Footer'
 import DocumentLanguage from '@/components/layout/DocumentLanguage'
@@ -31,12 +29,9 @@ export default async function LocaleLayout({ children, params }: Props) {
   return (
     <NextIntlClientProvider messages={messages}>
       <DocumentLanguage />
-      <GridOverlayProvider>
-        <Navigation />
-        <main>{children}</main>
-        <Footer />
-        <GridOverlay />
-      </GridOverlayProvider>
+      <Navigation />
+      <main>{children}</main>
+      <Footer />
     </NextIntlClientProvider>
   )
 }
